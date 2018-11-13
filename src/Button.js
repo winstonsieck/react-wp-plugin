43fd8c84af486215cda9dc6f72127ca691ca5dc9
winstonsieck <sieck@globalcognition.org>
2018-11-13 17:41:57 -0500

    delete scss & add in simple react lines

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import './button.scss';
+//import './button.scss';
 
 const { Component } = wp.element;
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+
+
+// const pEl = wp.element.createElement("p", {}, "Hello wp from React.");
+// wp.element.render(pEl, document.querySelector( '.entry-content' ) );
+
+// const Hello = () => <p>Hello World</p>;
+// wp.element.render(<Hello />, document.querySelector( '.entry-content' ) );
+
+
 import Button from './Button';
 
-wp.editor.render(<Button />, document.querySelector( '.entry-content' ) );
+wp.element.render(<Button />, document.querySelector( '.entry-content' ) );
